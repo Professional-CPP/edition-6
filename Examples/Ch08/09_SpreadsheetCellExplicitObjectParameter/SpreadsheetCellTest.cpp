33c7148b46bf2cc786bb92c87eee36f0a979191d
Marc Gregoire <marc.gregoire@nuonsoft.com>
2023-06-10 18:21:22 +0200

    Fixed 09_SpreadsheetCellExplicitObjectParameter files.

--- Examples/Ch08/09_SpreadsheetCellExplicitObjectParameter/SpreadsheetCellTest.cpp
+++ Examples/Ch08/09_SpreadsheetCellExplicitObjectParameter/SpreadsheetCellTest.cpp
@@ -1,62 +1,7 @@
-//import spreadsheet_cell;
+import spreadsheet_cell;
 import std;
 
 using namespace std;
-
-class SpreadsheetCell
-{
-public:
-	void setValue(this SpreadsheetCell& self, double value);
-	double getValue() const;
-
-	void setString(std::string_view value);
-	std::string getString() const;
-
-private:
-	std::string doubleToString(double value) const;
-	double stringToDouble(std::string_view value) const;
-
-	double m_value{ 0 };
-};
-
-void printCell(const SpreadsheetCell& cell)
-{
-	println("{}", cell.getString());
-}
-
-void SpreadsheetCell::setValue(this SpreadsheetCell& self, double value)
-{
-	self.m_value = value;
-	printCell(self);
-}
-
-double SpreadsheetCell::getValue() const
-{
-	return m_value;
-}
-
-void SpreadsheetCell::setString(string_view value)
-{
-	m_value = stringToDouble(value);
-}
-
-string SpreadsheetCell::getString() const
-{
-	return doubleToString(m_value);
-}
-
-string SpreadsheetCell::doubleToString(double value) const
-{
-	return to_string(value);
-}
-
-double SpreadsheetCell::stringToDouble(string_view value) const
-{
-	double number{ 0 };
-	from_chars(value.data(), value.data() + value.size(), number);
-	return number;
-}
-
 
 int main()
 {
