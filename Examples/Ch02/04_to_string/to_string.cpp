ab505a6ec6afe4b9c77470473900ac638c8f4ca4
Marc Gregoire <marc.gregoire@nuonsoft.com>
2023-09-18 18:51:52 +0200

    Fixed a comment.

--- Examples/Ch02/04_to_string/to_string.cpp
+++ Examples/Ch02/04_to_string/to_string.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
 	long double d{ 3.14L };
-	string s{ to_string(d) };  // s will contain 3.140000
+	string s{ to_string(d) };  // s contains 3.140000
 	println("{}", s);
 }
