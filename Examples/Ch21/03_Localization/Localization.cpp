52b5dc55040d22e37d51388e1d71e3d1a6bd2b3b
Marc Gregoire <marc.gregoire@nuonsoft.com>
2023-10-22 09:55:51 +0200

    Changed size_t to unsigned.

--- Examples/Ch21/03_Localization/Localization.cpp
+++ Examples/Ch21/03_Localization/Localization.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	size_t n{ 5 };
+	unsigned n{ 5 };
 	wstring filename{ L"file1.txt" };
 	wcout << n << L" bytes read from " << filename << endl;
 	wcout << n << L" Bytes aus " << filename << L" gelezen" << endl;
