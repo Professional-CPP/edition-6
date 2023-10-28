9c3f7af8f774eb6219bd1542cf09ca157161de6a
Marc Gregoire <marc.gregoire@nuonsoft.com>
2023-10-28 11:35:22 +0200

    Addressed code review comments.

--- Examples/Ch27/03_mutex/01_spinlock.cpp
+++ Examples/Ch27/03_mutex/01_spinlock.cpp
@@ -2,26 +2,26 @@ import std;
 
 using namespace std;
 
-atomic_flag spinlock;
-static const size_t NumberOfThreads{ 50 };
-static const size_t LoopsPerThread{ 100 };
+static constexpr unsigned NumberOfThreads{ 50 };
+static constexpr unsigned LoopsPerThread{ 100 };
 
-void dowork(size_t threadNumber, vector<size_t>& data)
+void dowork(unsigned threadNumber, vector<unsigned>& data, atomic_flag& spinlock)
 {
-	for (size_t i{ 0 }; i < LoopsPerThread; ++i) {
-		while (spinlock.test_and_set()) {}  // Spins until lock is acquired.
+	for (unsigned i{ 0 }; i < LoopsPerThread; ++i) {
+		while (spinlock.test_and_set()) {} // Spins until lock is acquired.
 		// Safe to handle shared data...
 		data.push_back(threadNumber);
-		spinlock.clear();                    // Releases the acquired lock.
+		spinlock.clear();                  // Releases the acquired lock.
 	}
 }
 
 int main()
 {
-	vector<size_t> data;
+	vector<unsigned> data;
+	atomic_flag dataSpinlock;
 	vector<jthread> threads;
-	for (size_t i{ 0 }; i < NumberOfThreads; ++i) {
-		threads.emplace_back(dowork, i, ref(data));
+	for (unsigned i{ 0 }; i < NumberOfThreads; ++i) {
+		threads.emplace_back(dowork, i, ref(data), ref(dataSpinlock));
 	}
 	for (auto& t : threads) {
 		t.join();
