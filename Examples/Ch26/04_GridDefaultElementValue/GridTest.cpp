2c847d4c8a8cff7919af0d4c069a6b1d38809987
Marc Gregoire <marc.gregoire@nuonsoft.com>
2023-11-01 13:36:08 +0100

    Addressed code review comments.

--- Examples/Ch26/04_GridDefaultElementValue/GridTest.cpp
+++ Examples/Ch26/04_GridDefaultElementValue/GridTest.cpp
@@ -3,7 +3,7 @@ import spreadsheet_cell;
 
 int main()
 {
-	Grid<int> myIntGrid;		// Initial value is 0
+	Grid<int> myIntGrid;		// Initial value is int{}, i.e., 0
 	Grid<int, 10> myIntGrid2;	// Initial value is 10
 
 	myIntGrid.at(1, 2) = 3;
