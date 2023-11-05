c324c132a0ae785c13921ce00c300b6448c0c720
Marc Gregoire <marc.gregoire@nuonsoft.com>
2023-11-05 11:19:00 +0100

    Switched to non-owning pointer, based on code review.

--- Examples/Ch33/01_DependencyInjection/LoggerTest.cpp
+++ Examples/Ch33/01_DependencyInjection/LoggerTest.cpp
@@ -1,19 +1,25 @@
 import logger;
+import std;
 
 using namespace std;
 
 class Foo
 {
 public:
-	explicit Foo(ILogger& logger) : m_logger{ logger } { }
+	explicit Foo(ILogger* logger) : m_logger{ logger }
+	{
+		if (m_logger == nullptr) {
+			throw invalid_argument("ILogger cannot be null.");
+		}
+	}
 
 	void doSomething()
 	{
-		m_logger.log("Hello dependency injection!", ILogger::LogLevel::Info);
+		m_logger->log("Hello dependency injection!", ILogger::LogLevel::Info);
 	}
 
 private:
-	ILogger& m_logger;
+	ILogger* m_logger;
 };
 
 int main()
@@ -21,6 +27,6 @@ int main()
 	Logger concreteLogger{ "log.out" };
 	concreteLogger.setLogLevel(ILogger::LogLevel::Debug);
 
-	Foo f{ concreteLogger };
+	Foo f{ &concreteLogger };
 	f.doSomething();
 }
