1c4979e8340a5a611bf27f83a35c2e702e9c1b46
Marc Gregoire <marc.gregoire@nuonsoft.com>
2023-11-08 16:10:15 +0100

    Added example for istreambuf_iterator.

---
+++ Examples/Ch17/04_StreamIterators/03_istreambuf_iterator.cpp
@@ -0,0 +1,17 @@
+import std;
+
+using namespace std;
+
+int main()
+{
+	ifstream inputFile{ "some_data.txt" };
+	if (inputFile.fail()) {
+		println(cerr, "Unable to open file for reading.");
+		return 1;
+	}
+	string fileContents{
+		istreambuf_iterator<char>{ inputFile },
+		istreambuf_iterator<char>{ }
+	};
+	println("{}", fileContents);
+}
