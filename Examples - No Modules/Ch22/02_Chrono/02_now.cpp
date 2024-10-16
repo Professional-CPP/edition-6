2ae090cc2b11517ad1df185da26226e630fcc937
Marc Gregoire <marc.gregoire@nuonsoft.com>
2024-10-16 19:51:40 +0200

    Added "No Modules" implementations of all examples in Chapter 22.

---
+++ Examples - No Modules/Ch22/02_Chrono/02_now.cpp
@@ -0,0 +1,16 @@
+#include <print>
+#include <chrono>
+#include <locale>
+
+using namespace std;
+using namespace std::chrono;
+
+int main()
+{
+	// Set the global locale to the user's local (see Chapter 21).
+	locale::global(locale{ "" });
+
+	// Print the current UTC time.
+	println("UTC: {:L}", system_clock::now());
+	println("UTC: {:L%c}", system_clock::now());
+}
