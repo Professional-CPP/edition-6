1ae82afeeab6ff36211ed61168ead05d7f25c6d5
Marc Gregoire <marc.gregoire@nuonsoft.com>
2024-12-08 10:06:09 +0100

    Added "No Modules" implementations of all examples in Chapter 31.

---
+++ Examples - No Modules/Ch31/06_ArticleCitations/04_AfterLogDebugging/ArticleCitationsTest.cpp
@@ -0,0 +1,26 @@
+#include "ArticleCitations.h"
+#include <print>
+#include <iostream>
+
+using namespace std;
+
+void processCitations(ArticleCitations cit)
+{
+	println("{}", cit.getArticle());
+	for (int i{ 0 }; i < cit.getNumCitations(); ++i) {
+		println("{}", cit.getCitation(i));
+	}
+}
+
+int main()
+{
+	while (true) {
+		print("Enter a file name (\"STOP\" to stop): ");
+		string filename;
+		cin >> filename;
+		if (filename == "STOP") { break; }
+
+		ArticleCitations cit{ filename };
+		processCitations(cit);
+	}
+}
