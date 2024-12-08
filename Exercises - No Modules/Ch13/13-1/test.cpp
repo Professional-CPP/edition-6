7f68a52ab8a5151fef5513cb6f079f1f479fc581
Marc Gregoire <marc.gregoire@nuonsoft.com>
2024-12-08 12:21:48 +0100

    Added "No Modules" implementations of all solutions for the exercises in Chapters 13-14.

---
+++ Exercises - No Modules/Ch13/13-1/test.cpp
@@ -0,0 +1,9 @@
+#include "Person.h"
+
+using namespace std;
+
+int main()
+{
+	Person person{ "John", "Doe" };
+	person.output();
+}
---
+++ Exercises - No Modules/Ch14/14-4/Spreadsheet.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "SpreadsheetCell.h"
+#include <cstddef>
+
+class Spreadsheet
+{
+public:
+	Spreadsheet(std::size_t width, std::size_t height);
+	Spreadsheet(const Spreadsheet& src);
+	Spreadsheet(Spreadsheet&& src) noexcept; // Move constructor
+	~Spreadsheet();
+
+	Spreadsheet& operator=(const Spreadsheet& rhs);
+	Spreadsheet& operator=(Spreadsheet&& rhs) noexcept;  // Move assignment
+
+	void setCellAt(std::size_t x, std::size_t y, const SpreadsheetCell& cell);
+	SpreadsheetCell& getCellAt(std::size_t x, std::size_t y);
+
+	void swap(Spreadsheet& other) noexcept;;
+	
+	static const std::size_t MaxHeight{ 100 };
+	static const std::size_t MaxWidth{ 100 };
+
+private:
+	void cleanup() noexcept;
+	void verifyCoordinate(std::size_t x, std::size_t y) const;
+
+	std::size_t m_width{ 0 };
+	std::size_t m_height{ 0 };
+	SpreadsheetCell** m_cells{ nullptr };
+};
+
+void swap(Spreadsheet& first, Spreadsheet& second) noexcept;
